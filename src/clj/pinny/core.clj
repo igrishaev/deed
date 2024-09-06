e7e1c691f6f2ee2ce1608fff0093ddc127b55404
Ivan Grishaev <ivan@grishaev.me>
2024-09-06 12:22:55 +0300

    updated

--- src/clj/pinny/core.clj
+++ src/clj/pinny/core.clj
@@ -2,7 +2,78 @@
   (:require
    [clojure.java.io :as io])
   (:import
-   (pinny Encoder Decoder EOF)))
+   (clojure.lang IPersistentVector
+                 IPersistentSet
+                 )
+   (pinny Encoder Decoder EOF OID
+          Encoder2
+
+          )))
+
+
+(defmulti -enc
+  (fn [encoder value]
+    (class value)))
+
+(defmulti -dec
+  (fn [decoder oid]
+    oid))
+
+(defn -encode-countable [^Encoder2 encoder oid len coll]
+  (reduce
+   (fn [acc item]
+     (+ acc (-enc encoder item)))
+   (.writeOID encoder oid)
+   coll))
+
+(defn -encode-multi [^Encoder2 encoder coll]
+  (reduce
+   (fn [acc item]
+     (+ acc (-enc encoder item)))
+   0
+   coll))
+
+(defmethod -enc String [^Encoder2 encoder ^String value]
+  (.encodeString encoder value))
+
+(defmethod -enc Integer [^Encoder2 encoder ^Integer value]
+  (.encodeInteger encoder value))
+
+(defmethod -enc Long [^Encoder2 encoder ^Long value ]
+  (.encodeLong encoder value))
+
+(defmethod -enc IPersistentVector [^Encoder2 encoder value]
+  (-encode-countable encoder OID/CLJ_VECTOR (count value) value))
+
+(defmethod -enc IPersistentSet [^Encoder2 encoder value]
+  (-encode-countable encoder 123 (count value) value))
+
+
+;; (defmethod -dec OID/INT [_ decoder]
+;;   (.readInt decoder))
+
+;; (defmethod -dec OID/LONG [_ decoder]
+;;   (.readLong decoder))
+
+
+(defmacro with-encoder2 [[bind dest] & body]
+  `(with-open [input# (io/output-stream ~dest)
+               ~bind (new Encoder2 input#)]
+     ~@body))
+
+
+(comment
+
+  (with-encoder2 [e (io/file "test.aaa")]
+    (-enc e 1)
+    (-enc e 2)
+    (-enc e 3))
+
+  (with-encoder2 [e (io/file "test.aaa")]
+    (-encode-multi e [1 2 3]))
+
+  )
+
 
 (defmacro with-encoder [[bind dest] & body]
   `(with-open [input# (io/output-stream ~dest)
---
+++ src/main/java/pinny/Encoder2.java
@@ -0,0 +1,118 @@
+package pinny;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPOutputStream;
+
+public final class Encoder2 implements AutoCloseable {
+
+    private final OutputStream outputStream;
+    private final byte[] buf;
+    private final ByteBuffer bb;
+
+    public Encoder2(final OutputStream outputStream) {
+        this(outputStream, Options.standard());
+    }
+
+    public Encoder2(final OutputStream outputStream, final Options options) {
+        buf = new byte[8];
+        bb = ByteBuffer.wrap(buf);
+        OutputStream destination = outputStream;
+        if (options.useGzip()) {
+            try {
+                destination = new GZIPOutputStream(destination);
+            } catch (IOException e) {
+                throw Error.error(e, "could not open Gzip output stream");
+            }
+        }
+        this.outputStream = new BufferedOutputStream(destination, Const.OUT_BUF_SIZE);
+    }
+
+    private int writeBuf(final int len) {
+        try {
+            outputStream.write(buf, 0, len);
+        } catch (IOException e) {
+            throw Error.error(e, "cannot write buffer, len: %s", len);
+        }
+        return len;
+    }
+
+    public int writeInt(final int i) {
+        bb.putInt(0, i);
+        return writeBuf(Const.INT_SIZE);
+    }
+
+    public int writeOID(final short oid) {
+        bb.putShort(0, oid);
+        return writeBuf(Const.OID_SIZE);
+    }
+
+    public int writeShort(final short s) {
+        bb.putShort(0, s);
+        return writeBuf(Const.SHORT_SIZE);
+    }
+
+    public int writeLong(final long l) {
+        bb.putLong(0, l);
+        return writeBuf(Const.LONG_SIZE);
+    }
+
+    public int writeFloat(final float f) {
+        bb.putFloat(0, f);
+        return writeBuf(Const.FLOAT_SIZE);
+    }
+
+    public int writeDouble(final double d) {
+        bb.putDouble(0, d);
+        return writeBuf(Const.DOUBLE_SIZE);
+    }
+
+    public int writeBoolean(final boolean bool) {
+        final byte b = bool ? (byte)1 : (byte)0;
+        bb.put(b);
+        return writeBuf(Const.BYTE_SIZE);
+    }
+
+    public long encodeInteger(final Integer i) {
+        return writeOID(OID.INT) + writeInt(i);
+    }
+
+    public long encodeLong(final Long l) {
+        return writeOID(OID.LONG) + writeLong(l);
+    }
+
+    public long encodeShort(final Short s) {
+        return writeOID(OID.INT) + writeShort(s);
+    }
+
+    public long encodeString(final String s) {
+        final byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        long sum = writeOID(OID.STRING);
+        sum += writeInt(bytes.length);
+        try {
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            throw Error.error(e, "count not write bytes, string: %s", s);
+        }
+        sum += bytes.length;
+        return sum;
+    }
+
+    public void flush() {
+        try {
+            outputStream.flush();
+        } catch (IOException e) {
+            throw Error.error(e, "could not flush the stream");
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            throw Error.error(e, "could not close the stream");
+        }
+    }
+}
