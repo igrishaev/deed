50214968b08ccb8cf596215b708d90e8edcd147e
Ivan Grishaev <ivan@grishaev.me>
2024-09-26 21:38:18 +0300

    refactor crypto streams

--- src/main/java/deed/Crypto.java
+++ src/main/java/deed/Crypto.java
@@ -10,13 +10,38 @@ import java.security.NoSuchAlgorithmException;
 
 public class Crypto {
 
-    public static SecretKey secretKey(final byte[] secret, final String transformation) {
-        final String[] parts = transformation.split("/");
-        if (parts.length == 0) {
-            throw Err.error("empty transformation: %s", transformation);
-        }
-        final String algorithm = parts[0];
-        return new SecretKeySpec(secret, algorithm);
+    public static final String AES_CBC_NoPadding = "AES/CBC/NoPadding";
+    public static final String AES_CBC_PKCS5Padding = "AES/CBC/PKCS5Padding";
+    public static final String AES_ECB_NoPadding = "AES/ECB/NoPadding";
+    public static final String AES_ECB_PKCS5Padding = "AES/ECB/PKCS5Padding";
+    public static final String AES_GCM_NoPadding = "AES/GCM/NoPadding";
+
+//    AES/CBC/NoPadding (128)
+//    AES/CBC/PKCS5Padding (128)
+//    AES/ECB/NoPadding (128)
+//    AES/ECB/PKCS5Padding (128)
+//    AES/GCM/NoPadding (128)
+//    DES/CBC/NoPadding (56)
+//    DES/CBC/PKCS5Padding (56)
+//    DES/ECB/NoPadding (56)
+//    DES/ECB/PKCS5Padding (56)
+//    DESede/CBC/NoPadding (168)
+//    DESede/CBC/PKCS5Padding (168)
+//    DESede/ECB/NoPadding (168)
+//    DESede/ECB/PKCS5Padding (168)
+//    RSA/ECB/PKCS1Padding (1024, 2048)
+//    RSA/ECB/OAEPWithSHA-1AndMGF1Padding (1024, 2048)
+//    RSA/ECB/OAEPWithSHA-256AndMGF1Padding (1024, 2048)
+
+    public static CipherInputStream AESInputStream(final InputStream in, final byte[] secret) {
+        return AESInputStream(in, secret, AES_CBC_NoPadding);
+    }
+
+    public static CipherInputStream AESInputStream(final InputStream in, final byte[] secret, final String transformation) {
+        final Key key = new SecretKeySpec(secret, "AES");
+        final Cipher cipher = cipher(transformation);
+        initDecrypt(cipher, key);
+        return new CipherInputStream(in, cipher);
     }
 
     public static Cipher cipher(final String transformation) {
@@ -45,25 +70,5 @@ public class Crypto {
         }
     }
 
-    public static CipherOutputStream wrapCipher(
-            final OutputStream out,
-            final String algorithm,
-            final byte[] secret
-    ) {
-        final Cipher cipher = cipher(algorithm);
-        final Key secretKey = secretKey(secret, algorithm);
-        initEncrypt(cipher, secretKey);
-        return new CipherOutputStream(out, cipher);
-    }
 
-    public static CipherInputStream wrapCipher(
-            final InputStream in,
-            final String algorithm,
-            final byte[] secret
-    ) {
-        final Cipher cipher = cipher(algorithm);
-        final Key secretKey = secretKey(secret, algorithm);
-        initDecrypt(cipher, secretKey);
-        return new CipherInputStream(in, cipher);
-    }
 }
